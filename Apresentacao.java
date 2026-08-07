f448e21d8653ce72fae461fdccc0becb14604df4
kauefilipe-cell <kaue.filipe@aluno.ifsp.edu.br>
2026-08-07 14:18:03 -0300

    Add files via upload

---
+++ Apresentacao.java
@@ -0,0 +1,5 @@
+class Apresentacao {
+	public static void main(String[] args) {
+		System.out.println("Ola, meu nome e " + args[0] + " e eu moro em " + args[1]);
+	}
+}
---
+++ MeusDados.java
@@ -0,0 +1,5 @@
+class MeusDados {
+	public static void main(String[] args) {
+		System.out.println("Kauê Filipe Nascimento de Oliveira\nTecnologia em Sistemas para Internet");
+	}
+}
